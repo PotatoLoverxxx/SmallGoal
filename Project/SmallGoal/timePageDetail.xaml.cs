6d5286ca6db7ae58736652c686a9ceb1e4808cbe
ieb <1@wa.vg>
2019-04-21 13:55:39 +0800

    basic navigation

---
+++ Project/SmallGoal/timePageDetail.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.Media.Playback;
+
+namespace SmallGoal
+{
+    /// <summary>
+    /// 时间管理详情页面
+    /// </summary>
+    public sealed partial class timePageDetail : Page
+    {
+        Models.MyGoalItem goalItem;
+
+        public timePageDetail()
+        {
+            this.InitializeComponent();
+        }
+
+        private void mainPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), "");
+        }
+
+        private void timePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(timePage), "");
+        }
+
+        private void planPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(planPage), "");
+        }
+
+        private void goBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(timePage));
+        }
+    }
+
+
+    /*------------------------- 转换器 ---------------------------*/
+    class DoubleToTimeSpan : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return TimeSpan.FromSeconds((double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ((TimeSpan)value).TotalSeconds;
+        }
+    }
+
+}
