1243b2e840e3c634c38c711659cd88f9a5b5d0c3
Limingfan2019 <lmf_2010@163.com>
2019-04-25 18:46:21 +0800

    databinding

--- Project/SmallGoal/planPage.xaml.cs
+++ Project/SmallGoal/planPage.xaml.cs
@@ -51,5 +51,59 @@ namespace SmallGoal
             time.DataContext = items;
             Frame.Navigate(typeof(timePage), "");
         }
+
+        /*-------------通过动态绑定显示右侧的item-----------------*/
+        private void calendar_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            if (calendar.SelectedDates.Count == 0) return;
+            DateTimeOffset sourceTime = calendar.SelectedDates[0];
+            ObservableCollection<Models.MyGoalItem> items = myViewModel.findDayGoalCollection(calendar.SelectedDates[0].Year, calendar.SelectedDates[0].Month, calendar.SelectedDates[0].Day);
+            time.ItemsSource = items;
+            name.ItemsSource = items;
+            totaltime.ItemsSource = items;
+
+            // 待完成的月计划
+            ObservableCollection<Models.MyGoalItem> monthItems = myViewModel.findMonthGoalCollection(calendar.SelectedDates[0].Year, calendar.SelectedDates[0].Month, 0);
+            monthPlan.ItemsSource = monthItems;
+            // 实际已完成的月计划
+            ObservableCollection<Models.MyGoalItem> realmonthItems = myViewModel.findMonthGoalCollection(calendar.SelectedDates[0].Year, calendar.SelectedDates[0].Month, 1);
+            realMonthPlan.ItemsSource = realmonthItems;
+
+            // 待完成的年计划
+            ObservableCollection<Models.MyGoalItem> yearItems = myViewModel.findYearGoalCollection(calendar.SelectedDates[0].Year, calendar.SelectedDates[0].Month, 0);
+            yearPlan.ItemsSource = yearItems;
+            // 实际完成的年计划
+            ObservableCollection<Models.MyGoalItem> realYealItems = myViewModel.findYearGoalCollection(calendar.SelectedDates[0].Year, calendar.SelectedDates[0].Month, 1);
+            realYealPlan.ItemsSource = realYealItems;
+        }
+
+        /*---------------- 根据计划类型改变右侧内容 -------------------*/
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dayPlanList.Visibility = Visibility.Collapsed;
+            monthPlanList.Visibility = Visibility.Collapsed;
+            yearPlanList.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dayPlanList.Visibility = Visibility.Collapsed;
+            monthPlanList.Visibility = Visibility.Visible;
+            yearPlanList.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            dayPlanList.Visibility = Visibility.Visible;
+            monthPlanList.Visibility = Visibility.Collapsed;
+            yearPlanList.Visibility = Visibility.Collapsed;
+        }
+
+        private void CalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            dayPlanList.Visibility = Visibility.Visible;
+            monthPlanList.Visibility = Visibility.Collapsed;
+            yearPlanList.Visibility = Visibility.Collapsed;
+        }
     }
 }
